c173116da2e5551a9c243c32fb0aed330816327c
dashuleka01 <dashka01012000@mail.ru>
2018-03-06 18:37:37 +0300

    Tests + peopleByStreet, peopleByHouse

--- src/AdressBookTest.java
+++ src/AdressBookTest.java
@@ -1,31 +1,61 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class AdressBookTest {
+    adress adress1 = new adress("Nevskiy", 54, 10);
+    adress adress2 = new adress("Moskovskiy", 10, 105);
+    adress adress3 = new adress("Nevskiy", 26, 541);
+    adress adress4 = new adress("Nevskiy", 26, 123);
+    String name = "Sasha";
+
     @Test
     void addPerson() {
-        adress adress1 = new adress("Nevskiy", 54, 10);
-        String name = "Sasha";
-        AdressBook book1 = new AdressBook();
-        //book1.book.put("", new adress("", 0, 0));
-        assertTrue(book1.addPerson(name, adress1).book.get(name) == adress1);
+        assertTrue(new AdressBook().addPerson(name, adress1).book.get(name) == adress1);
     }
 
     @Test
     void deletePerson() {
-        adress adress1 = new adress("Nevskiy", 54, 10);
-        String name = "Sasha";
         assertFalse(new AdressBook().deletePerson(name).book.containsKey(name));
     }
 
     @Test
     void changeAdress() {
+        assertFalse(new AdressBook().changeAdress(name, adress2).book.get(name) == adress2);
+        assertTrue(new AdressBook().addPerson(name, adress1).changeAdress(name, adress2).book.get(name) == adress2);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getAdress() {
+        assertEquals(adress1, new AdressBook().addPerson(name, adress1).getAdress(name));
+        assertNotEquals(adress1, new AdressBook().addPerson(name, adress2).getAdress(name));
     }
 
+    @Test
+    void peopleByStreet() {
+        AdressBook myBook = new AdressBook();
+        myBook.addPerson("Sasha", adress1);
+        myBook.addPerson("Dasha", adress2);
+        myBook.addPerson("Masha", adress3);
+        myBook.addPerson("Natasha", adress4);
+
+        assertEquals(new HashSet<>(Arrays.asList("Natasha", "Masha", "Sasha")), myBook.peopleByStreet("Nevskiy"));
+        assertNotEquals(new HashSet<>(Arrays.asList("Dasha", "Masha", "Sasha")), myBook.peopleByStreet("Nevskiy"));
+    }
+
+    @Test
+    void peopleByHouse() {
+        AdressBook myBook = new AdressBook();
+        myBook.addPerson("Sasha", adress1);
+        myBook.addPerson("Dasha", adress2);
+        myBook.addPerson("Masha", adress3);
+        myBook.addPerson("Natasha", adress4);
+
+        assertEquals(new HashSet<>(Arrays.asList("Natasha", "Masha")), myBook.peopleByHouse("Nevskiy", 26));
+        assertEquals(new HashSet<>(Arrays.asList("Sasha")), myBook.peopleByHouse("Nevskiy", 54));
+        assertNotEquals(new HashSet<>(Arrays.asList("Dasha")), myBook.peopleByHouse("Nevskiy", 54));
+    }
 }
--- src/adressbook.java
+++ src/adressbook.java
@@ -1,61 +1,83 @@
-
-import java.util.Map;
+import java.util.*;
 
 public class AdressBook {
 
     Map<String, adress> book;
 
     public AdressBook() {
-        this.book = book;
+        this.book = new HashMap<>();
     }
 
     //добавление пары человек-адрес
     public AdressBook addPerson(String person, adress ad) {
-        this.book.put(person, ad);
+        try {
+            this.book.put(person, ad);
+        }
+        catch (Exception e){
+            System.out.println("Please, try another name");
+        }
         return this;
     }
 
     //удаление человека
     public AdressBook deletePerson(String person) {
-        this.book.remove(person);
+        try {
+            this.book.remove(person);
+        }
+        catch (Exception e){
+            System.out.println("Please, try another name");
+        }
         return this;
     }
 
     //изменение адреса
     public AdressBook changeAdress(String person, adress ad){
-        this.book.replace(person, ad);
+        try {
+            this.book.replace(person, ad);
+        }
+        catch (Exception e){
+            System.out.println("Please, try another name");
+        }
         return this;
     }
 
     //получение адреса
     public adress getAdress(String person){
+        if (!this.book.containsKey(person)){
+            System.out.println("Please, try another name");
+            throw new IllegalArgumentException();
+        }
         return this.book.get(person);
     }
 
     //получение списка людей по улице
-
+    public Set<String> peopleByStreet(String sourceStreet){
+        Set<String> listOfNames = new HashSet<>();
+        for (String i:
+             this.book.keySet()) {
+            if (this.getAdress(i).street == sourceStreet)
+                listOfNames.add(i);
+        }
+        return listOfNames;
+    }
 
     //получение списка людей по дому
-
-
-
-
-
-
-    public static void main(String[] args) {
-        adress adress1 = new adress("Nevskiy", 54, 10);
-        AdressBook book1 = new AdressBook();
-        book1.addPerson("Sasha", adress1);
-        adress adress2 = new adress("Moskovskiy", 12, 32);
-        book1.addPerson("Masha", adress2);
+    public Set<String> peopleByHouse(String sourceStreet, int sourceHouse) {
+        Set<String> listOfNames = this.peopleByStreet(sourceStreet);
+        for (String i:
+             this.book.keySet()) {
+            if (this.getAdress(i).house != sourceHouse)
+                listOfNames.remove(i);
+        }
+        return listOfNames;
     }
 }
 
-
+//дополнительный класс
 class adress {
-    private String street;
-    private int house;
-    private int flat;
+    String street;
+    int house;
+    int flat;
 
     public adress(String street, int house, int flat) {
         this.street = street;
